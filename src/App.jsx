43e7a472a38c08e10258cf15f8dbfc665d1a603c
Zarnab Ali <zarnabalibhatti@gmail.com>
2025-03-13 01:37:05 +0500

    Updated features/login + register final

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/Loginpage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
